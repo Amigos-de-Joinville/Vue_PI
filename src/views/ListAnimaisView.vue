a2b9f11ee727dc3a7e02b57a88b736e709a770f5
Ana <aninha10.desouza@gmail.com>
2023-10-03 15:19:32 -0300

    Cards puxando o bakc

--- src/views/ListAnimaisView.vue
+++ src/views/ListAnimaisView.vue
@@ -22,36 +22,35 @@ onMounted(() => {
   buscarAnimais();
 });
 </script>
-<template >
+
+<template>
   <CabecalhoComp></CabecalhoComp>
-  <div class="row row-cols-1 row-cols-md-4 g-4" v-for="animal in animais"
-    :key="animal.id"
-    style="padding: 200px"
-    display="flex" >
-    <div class="col">
+  <main class="main"  >
+  <div class="scroll-view">
+  <div class="row row-cols-3 row-cols-md-6 g-6" style="padding-top: 20px">
+    <div v-for="animal in animais" :key="animal.id" class="col">
       <div class="card h-100">
         <img
           :src="animal.foto"
-          class="card-img-top"
-          alt="Los Angeles Skyscrapers"
+          class="foto"
+          alt="Foto do animal"
         />
         <div class="card-body">
           <h5 class="card-title">{{ animal.nome }}</h5>
-          <p class="card-text">
-            {{ animal.raca }}
-          </p>
+          <p class="card-text">{{ animal.raca }}</p>
         </div>
         <div class="card-footer">
-          <small class="text-muted">Atualizado à 2 dias</small>
+          <small class="text-muted">Atualizado há 2 dias</small>
           <i style="padding-left: 160px" class="far fa-heart fa-2x"></i>
         </div>
-        
         <ButtomComp></ButtomComp>
       </div>
     </div>
   </div>
-
+  </div>
+  </main>
 </template>
+
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Bangers&display=swap");
 .video {
@@ -68,5 +67,17 @@ h1 {
 .according-button {
   color: yellow;
 }
-
+.foto{
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+.scroll-view{
+  padding-top: 20px;
+}
+.main{
+  padding-top: 70px;
+  height: 50px;
+  width: ;
+}
 </style>
